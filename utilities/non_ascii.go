ea9e59f7f274a0cb58c6cfe9521c658870f32f8b
josopondo <josephineopondo5@gmail.com>
2024-05-08 15:47:37 +0300

    FEAT: print non_ascii character in error message

--- utilities/non_ascii.go
+++ utilities/non_ascii.go
@@ -1,18 +1,22 @@
 package utilities
 
-import "strings"
+import (
+	"strings"
+	// "unicode/utf8"
+)
 
 func NonAsciiOutput(input []string) string {
 	var art_work strings.Builder
-
 	non_ascii_map := NonAsciiMap(input)
+	printedChars := make(map[rune]bool)
 
 	for _, word := range input {
 
 		var builder strings.Builder
 		for _, char := range word {
-			if _, ok := non_ascii_map[char]; ok {
+			if _, ok := non_ascii_map[char]; ok && !printedChars[char] {
 				builder.WriteString("The non ascii character "+ string(char) +" was not printed\n")
+				printedChars[char] = true
 			}
 		}
 		art_work.WriteString(builder.String())

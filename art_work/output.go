e071ae3108dfb17a560ee9867a355eb098e4fc1d
josie-opondo <josephineopondo5@gmail.com>
2024-05-14 09:59:25 +0300

    reverted output.go to previous code

--- art_work/output.go
+++ art_work/output.go
@@ -8,22 +8,18 @@ func Output(input, fileContents []string) string {
 	var art_work strings.Builder
 	ascii_map := AsciiMap(fileContents)
 
+	var height int
 	for _, word := range input {
-		nonAsciis := make(map[rune]string)
-		for i := 0; i < 8; i++ {
+		if word == "" {
+			height = 1
+		} else {
+			height = 8
+		}
+		for i := 0; i < height; i++ {
 			var builder strings.Builder
 			for _, char := range word {
 				if ascii, ok := ascii_map[char]; ok {
 					builder.WriteString(fileContents[ascii+i])
-				} else {
-					if _, found := nonAsciis[char]; !found {
-						nonAsciis[char] = string(char)
-					}
-				}
-			}
-			if i == 4 {
-				for _, char := range nonAsciis {
-					builder.WriteString(char)
 				}
 			}
 			art_work.WriteString(builder.String())

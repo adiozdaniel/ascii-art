4040b4a558bbe9a966f784cf19498b0fb51e254c
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-05-14 06:49:07 +0300

    Corrected the plurals to reflect better user experience

--- utilities/non_ascii.go
+++ utilities/non_ascii.go
@@ -15,11 +15,12 @@ func NonAsciiOutput(input []string) string {
 
 	count := len(nonAsciiMap)
 	word := "character"
-	plural := "was"
+	plurals := []string{"This", "was"}
 	if count > 1 {
 		word = "characters"
-		plural = "were"
+		plurals[1] = "were"
+		plurals[0] = "These"
 	}
-	artWork.WriteString(fmt.Sprintf("These %d %s %s printed only once!\n", count, word, plural))
+	artWork.WriteString(fmt.Sprintf("%s %s %s printed only once!\n", plurals[0], word, plurals[1]))
 	return artWork.String()
 }

e43a9c2e0fc011c4bd3392a067ea42b2eed57c56
aosindo <andyovvo8@gmail.com>
2024-06-05 09:49:47 +0300

    modified to suit output

--- ascii-fs/main.go
+++ ascii-fs/main.go
@@ -19,57 +19,61 @@ func main() {
 }
 
 func Channel() {
+	file := "banner.txt"
 	reff := ""
 	str := ""
-	color := ""
-	banner := ""
+	color := "white"
+	banner := "standard"
+	out := false
 	if len(os.Args) == 1 || len(os.Args) > 5 {
-		fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+		fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 		os.Exit(0)
 	}
-	if strings.Contains(os.Args[1], "--color=") {
-		// if len(os.Args) == 1 || len(os.Args) > 5 {
-		// 	fmt.Println("err")
-		// 	os.Exit(1)
-		// }
-		if len(os.Args) == 5 && (os.Args[4] == "-standard" || os.Args[4] == "-thinkertoy" || os.Args[4] == "-shadow") {
+
+	if strings.Contains(os.Args[1], "--output=") {
+		out = true
+		if len(os.Args) == 4 {
+			file = os.Args[1][9:]
+			str = os.Args[2]
+			banner = os.Args[3]
+		}
+		if len(os.Args) == 3 {
+			file = os.Args[1][9:]
+			str = os.Args[2]
+		}
+	} else if strings.Contains(os.Args[1], "--color=") {
+		if len(os.Args) == 5 && (os.Args[4] == "standard" || os.Args[4] == "thinkertoy" || os.Args[4] == "shadow") {
 			color = os.Args[1][8:]
 			reff = os.Args[2]
 			str = os.Args[3]
 			banner = os.Args[4]
-		} else if len(os.Args) == 4 && (os.Args[3] == "-standard" || os.Args[3] == "-thinkertoy" || os.Args[3] == "-shadow") {
+		} else if len(os.Args) == 4 && (os.Args[3] == "standard" || os.Args[3] == "thinkertoy" || os.Args[3] == "shadow") {
 			color = os.Args[1][8:]
 			reff = os.Args[2]
 			str = os.Args[2]
 			banner = os.Args[3]
-		} else if len(os.Args) == 4 && os.Args[3] != "-standard" && os.Args[3] != "-thinkertoy" && os.Args[3] != "-shadow" {
+		} else if len(os.Args) == 4 && os.Args[3] != "standard" && os.Args[3] != "thinkertoy" && os.Args[3] != "shadow" {
 			color = os.Args[1][8:]
 			reff = os.Args[2]
 			str = os.Args[3]
-			banner = "-standard"
+			banner = "standard"
 		} else if len(os.Args) == 3 {
 			color = os.Args[1][8:]
 			reff = os.Args[2]
 			str = os.Args[2]
-			banner = "-standard"
+			banner = "standard"
 		} else {
-			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 			os.Exit(0)
 		}
-	}
-	if !strings.Contains(os.Args[1], "--color=") {
+	} else if !strings.Contains(os.Args[1], "--color=") {
 		if len(os.Args) == 1 || len(os.Args) > 3 {
-			fmt.Println("Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\"")
+			fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard")
 			os.Exit(0)
 		}
 		if len(os.Args) == 2 {
-			color = "white"
-			reff = os.Args[1]
 			str = os.Args[1]
-			banner = "-standard"
 		} else if len(os.Args) == 3 {
-			color = "white"
-			reff = os.Args[1]
 			str = os.Args[1]
 			banner = os.Args[2]
 		}
@@ -87,5 +91,18 @@ func Channel() {
 	input := strings.ReplaceAll(str, "\\n", "\n")
 	output := ascii.Output(color, reff, strings.Split(input, "\n"), fileContents)
 	nonAsciis := utils.NonAsciiOutput(strings.Split(input, "\n"))
-	fmt.Print(output, nonAsciis)
+	// fmt.Print(output, nonAsciis)
+	// os.Remove("./banner.txt")
+
+	if out {
+		if strings.HasSuffix(file, ".txt") {
+			os.WriteFile(file, []byte(output+nonAsciis+"\n"), 0o644)
+		} else {
+			os.Remove("./banner.txt")
+			fmt.Println("Please input a .txt file")
+		}
+	} else {
+		fmt.Print(output, nonAsciis)
+		os.Remove("./banner.txt")
+	}
 }

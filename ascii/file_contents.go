b935f2b268dfd517fe86289fbffb479053aca7df
adaniel <adiozdaniel@gmail.com>
2024-06-20 11:45:02 +0300

    feat: added support for banner file validation - added a new banner file. The banner file is not yet ready for use

--- ascii/file_contents.go
+++ ascii/file_contents.go
@@ -1,6 +1,8 @@
 package ascii
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
 	"os"
 	"strings"
@@ -9,13 +11,32 @@ import (
 	utils "github.com/adiozdaniel/ascii-art/utils"
 )
 
+const (
+	standard   = "e194f1033442617ab8a78e1ca63a2061f5cc07a3f05ac226ed32eb9dfd22a6bf"
+	thinkertoy = "64285e4960d199f4819323c4dc6319ba34f1f0dd9da14d07111345f5d76c3fa3"
+	shadow     = "26b94d0b134b77e9fd23e0360bfd81740f80fb7f6541d1d8c5d85e73ee550f73"
+)
+
+//validateBanner calculates the characters to verify the validity of the banner file
+func validateBanner(banner []byte) bool {
+	hasher := sha256.New()
+	hasher.Write(banner)
+	hash := hex.EncodeToString(hasher.Sum(nil))
+
+	if hash == standard || hash == thinkertoy || hash == shadow {
+		return true
+	}
+	return false
+}
+
 // FileContents returns a slice of strings containing ascii artwork characters
 func FileContents() []string {
 	fileName, _ := utils.GetFile()
 	contents, err := os.ReadFile(fileName)
 	var ans string
-	if err != nil {
-		fmt.Printf("File %v has a problem, would you wish to download it (yes/no): ", fileName[11:])
+
+	if err != nil || !validateBanner(contents){
+		fmt.Printf("'%v' is either missing or corrupted\nDo you wish to download it (yes/no): ", fileName[11:])
 		fmt.Scan(&ans)
 		if ans == "yes" {
 			fmt.Print("Be patient while downloading...\n")
@@ -27,22 +48,19 @@ func FileContents() []string {
 				os.Exit(0)
 			}
 			fmt.Print("\033[2A")
-			// Clear the current line
 			fmt.Print("\033[2K")
-			// Move to the beginning of the line
 			fmt.Print("\033[G")
-			// Clear the next line
 			fmt.Print("\033[2K")
 			fmt.Println("Download Complete, This is awesome right?😎😎😎")
 			fmt.Print("\033[2K")
 			time.Sleep(2 * time.Second)
 			fmt.Print("\033[1A")
-			// Clear the current line
+			fmt.Print("\033[2K")
+			fmt.Print("\033[1A")
 			fmt.Print("\033[2K")
 
-			// fmt.Print("\033c")
 		} else {
-			fmt.Println("Program stopped: No banner file")
+			fmt.Println("The banner file is missing or corrupted!")
 			os.Exit(0)
 		}
 		contents, err = os.ReadFile(fileName)

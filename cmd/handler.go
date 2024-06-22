ca4ef3b7591a7c1aeaa88c5091027993bc297b02
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-06-22 17:08:38 +0300

    feat: added handlers in the cmd directory

---
+++ cmd/handler.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"net/http"
+	"strings"
+
+	ascii "github.com/adiozdaniel/ascii-art/ascii"
+	utils "github.com/adiozdaniel/ascii-art/utils"
+)
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	err := tmpl2.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
+	textInput := r.FormValue("textInput")
+	fileName := r.FormValue("FileName")
+
+	fileContents := ascii.FileContents()
+	input := utils.Inputs.Input
+	output := ascii.Output(fileContents)
+	nonAsciis := utils.NonAsciiOutput(strings.Split(input, "\n"))
+
+	fileContents := ascii.FileContents()
+	input := strings.ReplaceAll(textInput, "\\n", "\n")
+	output := ascii.Output(strings.Split(input, "\n"), fileContents)
+	nonAsciis := utils.NonAsciiOutput(strings.Split(input, "\n"))
+	result := output + "\n\n" + nonAsciis
+
+	// fmt.Printf("You typed: %s\nYou selected: %s", textInput, fileName)
+	data := struct {
+		Body string
+	}{
+		Body: result,
+	}
+
+	tmpl2.Execute(w, data)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"strings"
+	"text/template"
 
-	"github.com/adiozdaniel/ascii-art/ascii"
-	"github.com/adiozdaniel/ascii-art/utils"
+	ascii "github.com/adiozdaniel/ascii-art/ascii"
+	utils "github.com/adiozdaniel/ascii-art/utils"
 )
 
-func main() {
-	fileContents := ascii.FileContents()
-	input := utils.Inputs.Input
-	output := ascii.Output(fileContents)
-	nonAsciis := utils.NonAsciiOutput(strings.Split(input, "\n"))
+var tmpl2 = template.Must(template.ParseFiles("index.html"))
 
-	fmt.Print(output, nonAsciis)
+func main() {
+
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/submit-form", submitHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+
+// 	"github.com/adiozdaniel/ascii-art/ascii"
+// 	"github.com/adiozdaniel/ascii-art/utils"
+// )
+
+// func main() {
+// 	fileContents := ascii.FileContents()
+// 	input := utils.Inputs.Input
+// 	output := ascii.Output(fileContents)
+// 	nonAsciis := utils.NonAsciiOutput(strings.Split(input, "\n"))
+
+// 	fmt.Print(output, nonAsciis)
+// }

c2dcf063df5da6198e9732016db7f23eaa26488e
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-06-23 06:51:50 +0300

    feat: incorporated both terminal and web functionality

--- cmd/main.go
+++ cmd/main.go
@@ -3,36 +3,47 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+
+	ascii "github.com/adiozdaniel/ascii-art/ascii"
+	utils "github.com/adiozdaniel/ascii-art/utils"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	if os.Args[1] == "-web" {
+		mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path == "/" {
-			homeHandler(w, r)
-		} else if r.URL.Path == "/submit-form" {
-			submitHandler(w, r)
-		} else {
-			notFoundHandler(w, r)
+		mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/" {
+				homeHandler(w, r)
+			} else if r.URL.Path == "/submit-form" {
+				submitHandler(w, r)
+			} else {
+				notFoundHandler(w, r)
+			}
+		})
+
+		mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
+		mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, "../static/favicon.ico")
+		})
+
+		server := &http.Server{
+			Addr:    ":8080",
+			Handler: mux,
 		}
-	})
 
-	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("../static"))))
-	mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "../static/favicon.ico")
-	})
-
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+		fmt.Println("Server is running on http://localhost:8080")
+		err := server.ListenAndServe()
+		if err != nil {
+			panic(err)
+		}
 	}
+	fileContents := ascii.FileContents()
+	output := ascii.Output(fileContents)
+	nonAsciis := utils.NonAsciiOutput()
 
-	fmt.Println("Server is running on http://localhost:8080")
-	err := server.ListenAndServe()
-	if err != nil {
-		panic(err)
-	}
+	fmt.Print(output, nonAsciis)
 }
 
 // func main() {

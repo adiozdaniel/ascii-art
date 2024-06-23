5e6929c0d080b3ab0cbf337d72588f743ac62ac8
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-06-23 07:08:50 +0300

    chores: removed unnecessary comments in the main

--- cmd/main.go
+++ cmd/main.go
@@ -45,30 +45,3 @@ func main() {
 
 	fmt.Print(output, nonAsciis)
 }
-
-// func main() {
-// 	http.HandleFunc("/", homeHandler)
-// 	http.HandleFunc("/submit-form", submitHandler)
-
-// 	fmt.Println("Server is running on http://localhost:8080")
-// 	http.ListenAndServe(":8080", nil)
-// }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"strings"
-
-// 	"github.com/adiozdaniel/ascii-art/ascii"
-// 	"github.com/adiozdaniel/ascii-art/utils"
-// )
-
-// func main() {
-// 	fileContents := ascii.FileContents()
-// 	input := utils.Inputs.Input
-// 	output := ascii.Output(fileContents)
-// 	nonAsciis := utils.NonAsciiOutput(strings.Split(input, "\n"))
-
-// 	fmt.Print(output, nonAsciis)
-// }

9f8ef0ba837d9d915705148fb7a06985463177c3
ADIOZ DANIEL <adiozdaniel@gmail.com>
2024-06-23 07:46:35 +0300

    chores: handled newlines

--- ascii/output.go
+++ ascii/output.go
@@ -14,22 +14,29 @@ func Output(fileContents []string) string {
 	var height int = 8
 	var color = utils.Inputs.Color
 	var reff = utils.Inputs.ColorRef
-	var word = utils.Inputs.Input
+	var input = strings.Split(strings.ReplaceAll(utils.Inputs.Input, "\\n", "\n"), "\n")
 
-	for i := 0; i < height; i++ {
-		var builder strings.Builder
-		for _, char := range word {
-			if ascii, ok := ascii_map[char]; ok {
-				colorCode := GetColorCode(color)
-				if strings.Contains(reff, string(char)) {
-					builder.WriteString(colorCode + fileContents[ascii+i] + reset)
-				} else {
-					builder.WriteString(fileContents[ascii+i])
+	for _, word := range input {
+		if word == "" {
+			height = 1
+		} else {
+			height = 8
+		}
+		for i := 0; i < height; i++ {
+			var builder strings.Builder
+			for _, char := range word {
+				if ascii, ok := ascii_map[char]; ok {
+					colorCode := GetColorCode(color)
+					if strings.Contains(reff, string(char)) {
+						builder.WriteString(colorCode + fileContents[ascii+i] + reset)
+					} else {
+						builder.WriteString(fileContents[ascii+i])
+					}
 				}
 			}
+			art_work.WriteString(builder.String())
+			art_work.WriteRune('\n')
 		}
-		art_work.WriteString(builder.String())
-		art_work.WriteRune('\n')
 	}
 
 	return art_work.String()

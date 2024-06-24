22766f30f4f5d761aa3e2400182a2d0be7fb5105
josopondo <josephineopondo5@gmail.com>
2024-06-24 08:12:28 +0300

    ref: changed from constants to maps for simplicity

--- utils/error_handler.go
+++ utils/error_handler.go
@@ -5,16 +5,16 @@ import (
 	"os"
 )
 
-//constant used as an output value in ErrorHandler
-const (
-	fatal   = "Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\" standard"
-	color   = "🤯 Oops!We couldn't recognise your color\n\nKindly search supported colors here: https://htmlcolorcodes.com/"
-	outputs = "🧐 Oops!We can't find your \"something\"\n\nEX: go run . [OPTION] [STRING] \"something\""
-	output  = "Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<banner.txt> \"something\" standard"
-)
+//errors is a map of error output value in ErrorHandler
+var errors = map[string]string{
+	"fatal":   "Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\" standard",
+	"color":   "🤯 Oops!We couldn't recognise your color\n\nKindly search supported colors here: https://htmlcolorcodes.com/",
+	"outputs": "🧐 Oops!We can't find your \"something\"\n\nEX: go run . [OPTION] [STRING] \"something\"",
+	"output":  "Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<banner.txt> \"something\" standard",
+}
 
 // ErrorHandler outputs errors and safely exits the program
 func ErrorHandler(errType string) {
-	fmt.Println(errType)
+	fmt.Println(errors[errType])
 	os.Exit(0)
 }

5d41b2786641a31c974d3c9e1ee9354f46a940c0
andyosyndoh <andyovvo8@gmail.com>
2024-07-01 17:48:47 +0300

    Feat: changed error message

--- utils/error_handler.go
+++ utils/error_handler.go
@@ -7,7 +7,7 @@ import (
 
 // errors is a map of error output value in ErrorHandler
 var errors = map[string]string{
-	"fatal":   "Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <letters to be colored> \"something\" standard",
+	"fatal":   "Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <substring to be colored> \"something\" standard",
 	"color":   "🤯 Oops! We couldn't recognise your color\n\nKindly search supported colors here: https://htmlcolorcodes.com/",
 	"outputs": "🧐 Oops! We can't find your \"something\"\n\nEX: go run . [OPTION] [STRING] \"something\"",
 	"output":  "Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<banner.txt> \"something\" standard",

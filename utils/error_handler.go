f136937c3633f109e935c92e63831b06a798507a
josie-opondo <josephineopondo5@gmail.com>
2024-07-08 09:59:47 +0300

    chore: changed EX to Example in output usage message

--- utils/error_handler.go
+++ utils/error_handler.go
@@ -11,7 +11,7 @@ var errors = map[string]string{
 	"colors": "Usage: go run . [OPTION] [STRING]\n\nEX: go run . --color=<color> <substring to be colored> \"something\" standard",
 	"justify":  "Usage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard",
 	"color":  "🤯 Oops! We couldn't recognise your color\n\nKindly search supported colors here: https://htmlcolorcodes.com/",
-	"output": "Usage: go run . [OPTION] [STRING] [BANNER]\n\nEX: go run . --output=<fileName.txt> something standard",
+	"output": "Usage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --output=<fileName.txt> something standard",
 	"txt":    "😣Oops! We currently only support text files\n\nSee Documentation in: ../README.md",
 	"web":    "😮Oops! Something went wrong",
 }

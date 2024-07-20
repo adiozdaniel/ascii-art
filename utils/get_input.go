3e8b9fbd87cffaf26e5db4f6758970ab1f18bf08
josie-opondo <josephineopondo5@gmail.com>
2024-07-20 17:38:03 +0300

    chore: added documentation for the CheckInput

--- utils/get_input.go
+++ utils/get_input.go
@@ -147,6 +147,7 @@ func init() {
 	}
 }
 
+// CheckInput checks if there is invalid input in the command line arguments.
 func CheckInput(input []string) {
 	for i, arg := range input {
 		if (strings.HasPrefix(arg, "-align=") || strings.HasPrefix(arg, "--align=")) && i+1 < len(input) && input[i+1] == "--" {

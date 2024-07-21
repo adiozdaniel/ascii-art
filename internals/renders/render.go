f6899c83c97ecc82047be806700665837bc9979f
adiozdaniel <adiozdaniel@gmail.com>
2024-07-21 20:59:27 +0300

    chore: added a helper function that will combine the layout and the html templates - this avoids runtime errors if the html files are tampered with during runtime - also makes the css and html statements reusable

--- internals/renders/render.go
+++ internals/renders/render.go
@@ -3,13 +3,18 @@ package renders
 import (
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 type FormData struct {
 	Body string
 }
 
+// Data is a global variable to hold the form data
 var Data FormData
+
+// functions is a map of template functions
+var functions = template.FuncMap{}
 
 // RenderTemplate is a helper function to render HTML templates
 func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
@@ -23,4 +28,36 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+// RenderTemplateString is a helper function to render HTML templates as strings
+func RenderTemplateString(w http.ResponseWriter, tmpl string) error {
+	myCache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./views/templates/*.page.html")
+	if err != nil {
+		return err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return err
+		}
+
+		matches, err := filepath.Glob("./views/templates/*.layout.html")
+		if err != nil {
+			return err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./views/templates/*.layout.html")
+			if err != nil {
+				return err
+			}
+		}
+
+		myCache[name] = ts
+	}
+	return nil
 }

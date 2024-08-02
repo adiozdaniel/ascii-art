1ac0b7dc503c0507fb689c7e3e00aa071bf6f763
adiozdaniel <adiozdaniel@gmail.com>
2024-08-02 19:27:47 +0300

    reff: removed the main.go in the cmd root - distributed the logic to web.go and cli.go

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/adiozdaniel/ascii-art/internals/routes"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	routes.RegisterRoutes(mux)
+
+	wrappedMux := routes.RouteChecker(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: wrappedMux,
+	}
+
+	utils.Inputs.BannerPath = "../banners/standard.txt"
+	utils.Inputs.Input = "Ascii~"
+	serverOutput := ascii.Output(utils.Inputs.Input)
+	fmt.Println(serverOutput + "=====================================\nserver running @http://localhost:8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		utils.ErrorHandler("web")
+	}
+}

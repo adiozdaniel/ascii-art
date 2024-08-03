d418a271571e20ab4ad01f15845ee9d635ac83c6
adiozdaniel <adiozdaniel@gmail.com>
2024-08-03 12:40:38 +0300

    fix: font path - to dynamically get the font to be used in the output

--- pkg/helpers/scan_input.go
+++ pkg/helpers/scan_input.go
@@ -1,5 +1,11 @@
 package helpers
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 // scanInput reads input from CLI interface and updates the input struct.
 func ScanInput(input string) {
 	cleanInput := app.RemoveQuotes(input)
@@ -14,35 +20,35 @@ func ScanInput(input string) {
 				app.Justify = alignment
 				continue
 			}
-			utils.ErrorHandler("justify")
+			app.ErrorHandler("justify")
 		case strings.Contains(word, "--color") || strings.Contains(word, "-color"):
 			color := strings.TrimPrefix(strings.TrimPrefix(word, "--color="), "-color=")
 			if color != "" {
 				app.Color = color
 				continue
 			}
-			utils.ErrorHandler("colors")
+			app.ErrorHandler("colors")
 		case strings.Contains(word, "--reff") || strings.Contains(word, "-reff"):
 			reff := strings.TrimPrefix(strings.TrimPrefix(word, "--reff="), "-reff=")
 			if reff != "" {
 				app.ColorRef = reff
 				continue
 			}
-			utils.ErrorHandler("colors")
+			app.ErrorHandler("colors")
 		case strings.Contains(word, "--output") || strings.Contains(word, "-output"):
 			fmt.Println("🙄 Sorry, FS Mode cannot be set in alignment mode.")
 			os.Exit(0)
 		case strings.Contains(word, "--standard") || strings.Contains(word, "--thinkertoy") || strings.Contains(word, "--shadow"):
-			if value, ok := utils.BannerFiles[word]; ok {
-				utils.Inputs.BannerPath = value
+			if value, ok := app.BannerFile[word]; ok {
+				app.Font = value
 				continue
 			}
 
 			newInput += word + " "
 		case isBannerFile(word):
 			if i == len(words)-1 && len(words) != 1 {
-				if value, ok := app.BannerFiles[word]; ok {
-					app.BannerPath = value
+				if value, ok := app.BannerFile[word]; ok {
+					app.Font = value
 				}
 				break
 			}
@@ -64,6 +70,6 @@ func isValidAlignment(alignment string) bool {
 
 // isBannerFile checks if the provided word is a banner file.
 func isBannerFile(word string) bool {
-	_, exists := utils.BannerFiles[word]
+	_, exists := app.BannerFile[word]
 	return exists
 }

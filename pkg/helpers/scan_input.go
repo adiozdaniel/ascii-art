4253968442596988fbc7e6170d2435417f3ee43f
adiozdaniel <adiozdaniel@gmail.com>
2024-08-03 18:28:26 +0300

    reff: removed unnecessary logic - for effeciency

--- pkg/helpers/scan_input.go
+++ pkg/helpers/scan_input.go
@@ -1,8 +1,6 @@
 package helpers
 
 import (
-	"fmt"
-	"os"
 	"strings"
 )
 
@@ -10,60 +8,9 @@ import (
 func ScanInput(input string) {
 	cleanInput := app.RemoveQuotes(input)
 	words := strings.Fields(cleanInput)
-	var newInput string
 
 	app.Args = append(app.Args, words...)
 	app.ParseArgs()
-
-	for i, word := range words {
-		switch {
-		case strings.Contains(word, "--align") || strings.HasPrefix(word, "-align"):
-			alignment := strings.TrimPrefix(strings.TrimPrefix(word, "--align="), "-align=")
-			if isValidAlignment(alignment) {
-				app.Align = alignment
-				continue
-			}
-			app.ErrorHandler("justify")
-		case strings.Contains(word, "--color") || strings.Contains(word, "-color"):
-			color := strings.TrimPrefix(strings.TrimPrefix(word, "--color="), "-color=")
-			if color != "" {
-				app.Color = color
-				continue
-			}
-			app.ErrorHandler("colors")
-		case strings.Contains(word, "--reff") || strings.Contains(word, "-reff"):
-			reff := strings.TrimPrefix(strings.TrimPrefix(word, "--reff="), "-reff=")
-			if reff != "" {
-				app.ColorRef = reff
-				continue
-			}
-			app.ErrorHandler("colors")
-		case strings.Contains(word, "--output") || strings.Contains(word, "-output"):
-			fmt.Println("🙄 Sorry, FS Mode cannot be set in alignment mode.")
-			os.Exit(0)
-		case strings.Contains(word, "--standard") || strings.Contains(word, "--thinkertoy") || strings.Contains(word, "--shadow"):
-			if value, ok := app.BannerFile[word]; ok {
-				app.Font = value
-				continue
-			}
-
-			newInput += word + " "
-		case isBannerFile(word):
-			if i == len(words)-1 && len(words) != 1 {
-				if value, ok := app.BannerFile[word]; ok {
-					app.Font = value
-				}
-				break
-			}
-			newInput += word + " "
-		default:
-			newInput += word + " "
-		}
-	}
-
-	if newInput != "" {
-		app.Input = strings.TrimSpace(newInput)
-	}
 }
 
 // isValidAlignment checks if the provided alignment is valid.

8b3977bbda2578a5b9a68f0b0ad24cfe90b93e69
adiozdaniel <adiozdaniel@gmail.com>
2024-08-03 08:27:46 +0300

    reff: added the necessary dependents to the web main()

--- cmd/web/web.go
+++ cmd/web/web.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/adiozdaniel/ascii-art/internals/ascii"
 	"github.com/adiozdaniel/ascii-art/internals/routes"
+	"github.com/adiozdaniel/ascii-art/pkg/utils"
+)
+
+// global variables
+var (
+	app = &utils.Inputs
 )
 
 func main() {
+	app.IsWeb = true
 	mux := http.NewServeMux()
 	routes.RegisterRoutes(mux)
 
@@ -18,9 +26,9 @@ func main() {
 		Handler: wrappedMux,
 	}
 
-	utils.Inputs.BannerPath = "../banners/standard.txt"
-	utils.Inputs.Input = "Ascii~"
-	serverOutput := ascii.Output(utils.Inputs.Input)
+	app.BannerPath = "../banners/standard.txt"
+	app.Input = "Ascii~"
+	serverOutput := ascii.Output(app.Input)
 	fmt.Println(serverOutput + "=====================================\nserver running @http://localhost:8080")
 	err := server.ListenAndServe()
 	if err != nil {

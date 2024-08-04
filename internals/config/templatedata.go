23ba6bfa65183982849d62aabaf863dab26276ba
adiozdaniel <adiozdaniel@gmail.com>
2024-08-04 22:59:58 +0300

    chore: remove unnecessary "body" - for code effeciency

--- internals/config/templatedata.go
+++ internals/config/templatedata.go
@@ -1,13 +1,11 @@
 package config
 
 type TemplateData struct {
-	Body      string            `json:"Body"`
 	StringMap map[string]string `json:"stringMap"`
 }
 
 func NewTemplateData() *TemplateData {
 	return &TemplateData{
-		Body:      "",
 		StringMap: make(map[string]string),
 	}
 }

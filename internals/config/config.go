f2e2d072dd4fed6032247b67c9d3f3f459ea30c5
adiozdaniel <adiozdaniel@gmail.com>
2024-08-04 14:25:00 +0300

    chore: create a method for caching banner resource files

--- internals/config/config.go
+++ internals/config/config.go
@@ -1,6 +1,10 @@
 package config
 
-import "html/template"
+import (
+	"fmt"
+	"html/template"
+	"path/filepath"
+)
 
 // AppConfig holds configuration for the application.
 type AppConfig struct {
@@ -12,6 +16,25 @@ type AppConfig struct {
 // App is the instance of AppConfig.
 func App() *AppConfig {
 	return &AppConfig{
-		TemplateCache: make(map[string]*template.Template),
+		TemplateCache:   make(map[string]*template.Template),
+		BannerFileCache: make(map[string]string),
+		UseCache:        false,
 	}
+}
+
+func (a *AppConfig) CreateTemplateCache() (map[string]string, error) {
+	myCache := make(map[string]string)
+	baseDir := instance.input.GetProjectRoot("views/static", "banners")
+
+	bannerDir := filepath.Join(baseDir, "*.txt")
+	banners, err := filepath.Glob(bannerDir)
+	if err != nil {
+		return myCache, fmt.Errorf("error globbing banners: %v", err)
+	}
+
+	for _, banner := range banners {
+		a.BannerFileCache[filepath.Base(banner)] = banner
+	}
+
+	return myCache, nil
 }

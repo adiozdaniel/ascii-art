2b1c799d515e7ca98728eb1206ee70198d462617
adiozdaniel <adiozdaniel@gmail.com>
2024-08-04 23:17:46 +0300

    chore: bind the handlers to the statemanager

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -27,19 +27,19 @@ func NewRepo(sm *models.StateManager) *Repository {
 }
 
 // HomeHandler handles the homepage route '/'
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) HomeHandler(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemplate(w, "home.page.html", nil)
 }
 
 // SubmitHandler handles the output route '/ascii-art'
-func SubmitHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) SubmitHandler(w http.ResponseWriter, r *http.Request) {
 	if r.FormValue("textInput") == "" && r.Method != "POST" {
 		renders.RenderTemplate(w, "ascii.page.html", nil)
 		return
 	}
 
 	if r.Method == "POST" && r.FormValue(("textInput")) == "" {
-		BadRequestHandler(w, r)
+		m.BadRequestHandler(w, r)
 		return
 	}
 
@@ -47,13 +47,13 @@ func SubmitHandler(w http.ResponseWriter, r *http.Request) {
 	banner := appData.BannerFile[r.FormValue("Font")]
 
 	if banner == "" {
-		BadRequestHandler(w, r)
+		m.BadRequestHandler(w, r)
 	}
 
 	appData.Flags["font"] = banner
 	err := helpers.FileContents(banner)
 	if err != nil {
-		NotFoundHandler(w, r)
+		m.NotFoundHandler(w, r)
 		return
 	}
 
@@ -67,29 +67,29 @@ func SubmitHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 // NotFoundHandler handles unknown routes; 404 status
-func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) NotFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	renders.RenderTemplate(w, "notfound.page.html", nil)
 }
 
 // BadRequestHandler handles bad requests routes
-func BadRequestHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) BadRequestHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusBadRequest)
 	renders.RenderTemplate(w, "badrequest.page.html", nil)
 }
 
 // ServerErrorHandler handles server failures that result in status 500
-func ServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) ServerErrorHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusInternalServerError)
 	renders.RenderTemplate(w, "serverError.page.html", nil)
 }
 
 // AboutHandler handles the about page route '/about'
-func AboutHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) AboutHandler(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemplate(w, "about.page.html", nil)
 }
 
 // ContactHandler handles the contact page route '/contact'
-func ContactHandler(w http.ResponseWriter, r *http.Request) {
+func (m *Repository) ContactHandler(w http.ResponseWriter, r *http.Request) {
 	renders.RenderTemplate(w, "contact.page.html", nil)
 }

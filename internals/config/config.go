2baf58c1770d8b5440ae1083c64bbd143344bf79
adiozdaniel <adiozdaniel@gmail.com>
2024-08-04 12:51:17 +0300

    chore: initialize appconfig

--- internals/config/config.go
+++ internals/config/config.go
@@ -6,3 +6,10 @@ import "html/template"
 type AppConfig struct {
 	TemplateCache map[string]*template.Template
 }
+
+// App is the instance of AppConfig.
+func App() *AppConfig {
+	return &AppConfig{
+		TemplateCache: make(map[string]*template.Template),
+	}
+}

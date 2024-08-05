ce33a6a7f5a89bc029322afa4d26b73935f43fd4
adiozdaniel <adiozdaniel@gmail.com>
2024-08-05 07:34:17 +0300

    chore: add a method for session creation

--- internals/models/sessionmanager.go
+++ internals/models/sessionmanager.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"sync"
+)
+
 // Session represents a user session
 type Session struct {
 	CRSFToken string
@@ -8,6 +12,7 @@ type Session struct {
 // SessionManager manages user sessions
 type SessionManager struct {
 	sessions map[string]*Session
+	lock     sync.RWMutex
 }
 
 // NewSessionManager returns a new instance of SessionManager
@@ -15,4 +20,22 @@ func NewSessionManager() *SessionManager {
 	return &SessionManager{
 		sessions: make(map[string]*Session),
 	}
+}
+
+// CreateSession creates a new session
+func (sm *SessionManager) CreateSession() *Session {
+	sm.lock.Lock()
+	defer sm.lock.Unlock()
+
+	sessionID := generateSessionID()
+	session := &Session{
+		CRSFToken: sessionID,
+	}
+	sm.sessions[sessionID] = session
+	return session
+}
+
+// generateSessionID generates a unique session ID
+func generateSessionID() string {
+	return "unique-session-id"
 }

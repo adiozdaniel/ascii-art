2a9086b92596e868958f2cb514fcdd898787b034
adiozdaniel <adiozdaniel@gmail.com>
2024-08-05 19:24:48 +0300

    fix: only redirect if the path is not 'login'

--- internals/middlewares/sessionmanager.go
+++ internals/middlewares/sessionmanager.go
@@ -20,8 +20,11 @@ func SessionMiddleware(sm *models.SessionManager) func(next http.Handler) http.H
 			if err == nil {
 				sessionID = cookie.Value
 			} else {
-				http.Redirect(w, r, "/login", http.StatusSeeOther)
-				return
+				if r.URL.Path != "/login" {
+					http.Redirect(w, r, "/login", http.StatusSeeOther)
+					return
+				}
+				sessionID = ""
 			}
 
 			session, exists := sm.GetSession(sessionID)
@@ -35,12 +38,16 @@ func SessionMiddleware(sm *models.SessionManager) func(next http.Handler) http.H
 					Secure:   false, // TODO replace with true for production
 				})
 
-				http.Redirect(w, r, "/login", http.StatusSeeOther)
-				return
+				if r.URL.Path != "/login" {
+					http.Redirect(w, r, "/login", http.StatusSeeOther)
+					return
+				}
 			}
 
-			ctx := context.WithValue(r.Context(), sm.SessionKey, session)
-			r = r.WithContext(ctx)
+			if session != nil {
+				ctx := context.WithValue(r.Context(), sm.SessionKey, session)
+				r = r.WithContext(ctx)
+			}
 
 			next.ServeHTTP(w, r)
 		})

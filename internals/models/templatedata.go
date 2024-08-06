c02d5dd208a8e98d76de3f99314aebfe0d76699d
adiozdaniel <adiozdaniel@gmail.com>
2024-08-06 20:00:12 +0300

    chore: add a method to template data

--- internals/models/templatedata.go
+++ internals/models/templatedata.go
@@ -1,5 +1,7 @@
 package models
 
+import "strings"
+
 type TemplateData struct {
 	StringMap map[string]string `json:"stringMap"`
 }
@@ -8,4 +10,13 @@ func NewTemplateData() *TemplateData {
 	return &TemplateData{
 		StringMap: make(map[string]string),
 	}
+}
+
+// CapitalizeFirst capitalizes the first character of a string and lowercases the rest.
+func (s *TemplateData) CapitalizeFirst(input string) string {
+	if len(input) == 0 {
+		return input
+	}
+
+	return strings.ToUpper(string(input[0])) + strings.ToLower(input[1:])
 }

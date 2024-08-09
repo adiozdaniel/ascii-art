68f82e9b3682fa34e3d3713a859161568420e4e0
adiozdaniel <adiozdaniel@gmail.com>
2024-08-09 14:04:24 +0300

    chore: load the initial data - to make sure the initial data is available before the cli fires up

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -29,8 +29,21 @@ func main() {
 	loadCli()
 }
 
+// init initializes the cli interface
 func init() {
-	NewCli(models.GetStateManager())
+	cli = *NewCli(models.GetStateManager())
+
+	bc, err := cli.app.GetConfig().CreateBannerCache()
+	if err != nil {
+		cli.app.GetInput().ErrorHandler("banners")
+	}
+
+	cli.app.GetConfig().BannerFileCache = bc
+
+	cli.app.GetInput().Flags["font"] = "--standard"
+	cli.app.GetInput().Flags["input"] = "Ascii~"
+	cli.app.GetInput().Flags["reff"] = "Ascii"
+	cli.app.GetInput().Flags["color"] = "#FABB60"
 }
 
 // loadCli runs the alignment mode of the application.

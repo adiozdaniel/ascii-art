a00ebd4a157821d03dbd8e9f3d7c7a8c00f2a2d0
adiozdaniel <adiozdaniel@gmail.com>
2024-08-10 08:20:34 +0300

    fix: errors to formErrors - to fix conflicts

--- internals/models/errors.go
+++ internals/models/errors.go
@@ -1,15 +1,15 @@
 package models
 
 // errors represents a collection of error messages for various fields
-type errors map[string][]string
+type formErrors map[string][]string
 
 // Add adds an error message to the specified field
-func (e *errors) Add(field, message string) {
-	e[field] = append(e[field], message)
+func (e *formErrors) Add(field, message string) {
+	(*e)[field] = append((*e)[field], message)
 }
 
 // Get returns the first error message for a given field, or an empty string if no errors exist
-func (e *errors) Get(field string) string {
+func (e *formErrors) Get(field string) string {
 	es := (*e)[field]
 	if len(es) == 0 {
 		return ""

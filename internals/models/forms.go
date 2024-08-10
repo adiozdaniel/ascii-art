491e6ca941c78c2e27e0f777dfa164af8d0ccdaa
adiozdaniel <adiozdaniel@gmail.com>
2024-08-10 08:35:27 +0300

    reff: group concerns - for readability

--- internals/models/forms.go
+++ internals/models/forms.go
@@ -5,17 +5,6 @@ import "net/url"
 // errors represents a collection of error messages for various fields
 type formErrors map[string][]string
 
-// Forms represents a collection of form data and errors
-type Forms struct {
-	url.Values
-	Errors formErrors
-}
-
-// NewForms creates a new Forms instance with the given data and initializes an empty error map
-func NewForms(data url.Values) *Forms {
-	return &Forms{data, make(formErrors)}
-}
-
 // Add adds an error message to the specified field
 func (e *formErrors) Add(field, message string) {
 	(*e)[field] = append((*e)[field], message)
@@ -28,4 +17,15 @@ func (e *formErrors) Get(field string) string {
 		return ""
 	}
 	return es[0]
+}
+
+// Forms represents a collection of form data and errors
+type Forms struct {
+	url.Values
+	Errors formErrors
+}
+
+// NewForms creates a new Forms instance with the given data and initializes an empty error map
+func NewForms(data url.Values) *Forms {
+	return &Forms{data, make(formErrors)}
 }

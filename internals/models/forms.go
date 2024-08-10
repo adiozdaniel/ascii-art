66ed48cc17316c8459fe7c2050848c897d147e18
adiozdaniel <adiozdaniel@gmail.com>
2024-08-10 08:31:11 +0300

    reff: move the errors into form.go - for code effeciency - for maintainability

--- internals/models/forms.go
+++ internals/models/forms.go
@@ -2,6 +2,9 @@ package models
 
 import "net/url"
 
+// errors represents a collection of error messages for various fields
+type formErrors map[string][]string
+
 // Forms represents a collection of form data and errors
 type Forms struct {
 	url.Values
@@ -11,4 +14,18 @@ type Forms struct {
 // NewForms creates a new Forms instance with the given data and initializes an empty error map
 func NewForms(data url.Values) *Forms {
 	return &Forms{data, make(formErrors)}
+}
+
+// Add adds an error message to the specified field
+func (e *formErrors) Add(field, message string) {
+	(*e)[field] = append((*e)[field], message)
+}
+
+// Get returns the first error message for a given field, or an empty string if no errors exist
+func (e *formErrors) Get(field string) string {
+	es := (*e)[field]
+	if len(es) == 0 {
+		return ""
+	}
+	return es[0]
 }

1d74e8307e2382fdee23b3c68858777f6242122e
adiozdaniel <adiozdaniel@gmail.com>
2024-08-10 08:27:26 +0300

    reff: add documentation - for maintainability - for readability

--- internals/models/forms.go
+++ internals/models/forms.go
@@ -2,11 +2,13 @@ package models
 
 import "net/url"
 
+// Forms represents a collection of form data and errors
 type Forms struct {
 	url.Values
 	Errors formErrors
 }
 
+// NewForms creates a new Forms instance with the given data and initializes an empty error map
 func NewForms(data url.Values) *Forms {
 	return &Forms{data, make(formErrors)}
 }

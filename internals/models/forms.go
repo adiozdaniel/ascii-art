d777a5ca6f86130f7700f51af0f726a0e90dabe2
adiozdaniel <adiozdaniel@gmail.com>
2024-08-11 13:39:05 +0300

    chore: make a formvalues type - for scalability

--- internals/models/forms.go
+++ internals/models/forms.go
@@ -27,16 +27,24 @@ func (e *formErrors) Get(field string) string {
 	return es[0]
 }
 
+// formValues represents a collection of form values submitted by the user
+type formValues map[string]string
+
+// Set sets the value of a form field
+func (v *Forms) Set(field, value string) {
+	v.FormValues[field] = value
+}
+
 // Forms represents a collection of form data and errors
 type Forms struct {
 	url.Values
 	Errors     formErrors
-	FormValues map[string]string
+	FormValues formValues
 }
 
 // NewForms creates a new Forms instance with the given data and initializes an empty error map
 func NewForms(data url.Values) *Forms {
-	return &Forms{data, make(formErrors), make(map[string]string)}
+	return &Forms{data, make(formErrors), make(formValues)}
 }
 
 // Has checks if a field has been submitted and returns true if it has, false otherwise
@@ -47,16 +55,6 @@ func (f *Forms) Has(field string, r *http.Request) bool {
 		return false
 	}
 	return r.Form.Get(field) != ""
-}
-
-// Set sets the value of a form field
-func (f *Forms) Set(field, value string) {
-	f.FormValues[field] = value
-}
-
-// Get returns the value of a form field, or an empty string if the field does not exist
-func (f *Forms) GetField(field string) string {
-	return f.FormValues[field]
 }
 
 // ValidateForm returns true if all fields in the form have been submitted and have no errors, false otherwise

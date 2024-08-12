2673eaed8c81b0939d7bae242b3e473f50ee83d5
adiozdaniel <adiozdaniel@gmail.com>
2024-08-12 08:13:56 +0300

    chore: tests routes - for authenticity

--- cmd/web/web_test.go
+++ cmd/web/web_test.go
@@ -26,3 +26,40 @@ func TestRunWeb(t *testing.T) {
 		t.Fatalf("Expected status OK, got %v", resp.Status)
 	}
 }
+
+// Test runs tests for routes.
+func TestRoutes(t *testing.T) {
+	handler, err := runWeb()
+	if err != nil {
+		t.Fatalf("Failed to run web: %v", err)
+	}
+
+	server := httptest.NewServer(handler)
+	defer server.Close()
+
+	tests := []struct {
+		path               string
+		expectedStatusCode int
+	}{
+		{"/", http.StatusOK},
+		{"/ascii-art", http.StatusOK},
+		{"/about", http.StatusOK},
+		{"/contact", http.StatusOK},
+		{"/login", http.StatusOK},
+		{"/logout", http.StatusSeeOther},
+		{"/error", http.StatusNotFound},
+		{"/nonexistent", http.StatusNotFound},
+	}
+
+	for _, tt := range tests {
+		resp, err := http.Get(server.URL + tt.path)
+		if err != nil {
+			t.Fatalf("Failed to make request to %v: %v", tt.path, err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != tt.expectedStatusCode {
+			t.Fatalf("For path %v, expected status %v, got %v", tt.path, tt.expectedStatusCode, resp.Status)
+		}
+	}
+}

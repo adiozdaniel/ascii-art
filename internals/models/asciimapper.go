6ffa76afc60e9e5a67ef198cabbf236377b63048
adiozdaniel <adiozdaniel@gmail.com>
2024-08-24 09:29:53 +0300

    create an ascii mapper - to get the location of the characters

---
+++ internals/models/asciimapper.go
@@ -0,0 +1,15 @@
+package models
+
+func AsciiMapper(fileContents []string) map[rune]int {
+	ascii_map := make(map[rune]int)
+	ascii := 32
+
+	for index, line := range fileContents {
+		if len(line) == 0 || len(line) == 1 {
+			ascii_map[rune(ascii)] = index + 1
+			ascii++
+		}
+	}
+
+	return ascii_map
+}

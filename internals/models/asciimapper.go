255d4d0970f17ebcdbabb8ce9d55474a124e48f2
adiozdaniel <adiozdaniel@gmail.com>
2024-08-24 09:46:35 +0300

    chores: add character builder

--- internals/models/asciimapper.go
+++ internals/models/asciimapper.go
@@ -1,5 +1,7 @@
 package models
 
+import "strings"
+
 func AsciiMapper(fileContents []string) map[rune]int {
 	ascii_map := make(map[rune]int)
 	ascii := 32
@@ -12,4 +14,24 @@ func AsciiMapper(fileContents []string) map[rune]int {
 	}
 
 	return ascii_map
+}
+
+var input = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+func CharacterBuilder(ascii_map map[rune]int, fileContents []string) string {
+	var character strings.Builder
+
+	for _, char := range input {
+		var builder strings.Builder
+		for i := 0; i < 8; i++ {
+			if ascii, ok := ascii_map[char]; ok {
+				builder.WriteString(fileContents[ascii+i])
+			}
+
+			builder.WriteString(builder.String())
+			builder.WriteRune('\n')
+		}
+		character.WriteRune('\n')
+	}
+	return character.String()
 }
